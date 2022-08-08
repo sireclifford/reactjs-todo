333e31885c28d79c73a9000047456527a1980ceb
sireclifford <dev.sire.clifford@gmail.com>
2022-08-08 21:07:04 +0000

    - create class components for the App - use react lifecycle to manage state and render the App - use inline styles, css modules to style the App - create methods to add, remove, and update todos

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styles from "./TodoItem.module.css";
+
+class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  };
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+
+  handleUpdatedDone = (event) => {
+   if (event.key === "Enter") {
+     this.setState({ editing: false });
+   }
+  };
+
+  render() {
+    const { completed, id, title } = this.props.todo;
+
+    let viewMode = {};
+    let editMode = {};
+
+    if (this.state.editing) {
+      viewMode.display = "none";
+    } else {
+      editMode.display = "none";
+    }
+
+    return (
+      <>
+        <li className={styles.item}>
+          <div onDoubleClick={this.handleEditing} style={viewMode}>
+            <input
+              type="checkbox"
+              className={styles.checkbox}
+              checked={completed}
+              onChange={() => this.props.handleChangeProps(id)}
+            />
+            <button onClick={() => this.props.deleteTodoProps(id)}>
+              Delete
+            </button>
+            <span className={completed ? styles.completedStyle : null}>
+              {title}
+            </span>
+          </div>
+          <input
+            type="text"
+            className={styles.textInput}
+            style={editMode}
+            value={title}
+            onChange={(e) => {
+              this.props.setUpdate(e.target.value, id);
+            }}
+            onKeyDown={this.handleUpdatedDone}
+          />
+        </li>
+      </>
+    );
+  }
+};
+
+export default TodoItem;
