064679a6ddf7b2e12d85c5de435a833a43c1ae64
sireclifford <dev.sire.clifford@gmail.com>
2022-08-09 08:40:38 +0000

    - refactor codes to use functional component - implement react hooks - persist state in local storage - routing and navigation using react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import React from 'react';
 import TodoContainer from "./components/TodoContainer";
+import { BrowserRouter } from "react-router-dom";
 
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>
 );
